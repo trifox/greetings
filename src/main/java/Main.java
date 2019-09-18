a2df1cb8d036e24532bf0dcc6a0ebd7878b22411
Christian Kleinhuis <ck@digitalgott.de>
2019-09-18 22:43:39 +0200

    initial setup with fakder :D

---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+import com.github.javafaker.*;
+
+import java.util.*;
+
+@FunctionalInterface
+interface StringCreator {
+    String createString();
+}
+
+public class Main {
+
+    public static void main(String args[]) {
+        Random random = new Random();
+
+        Faker faker = new Faker();
+        StringCreator x = faker.chuckNorris()::fact;
+        List<StringCreator> stringCreator = new ArrayList<>();
+
+        stringCreator.add(faker.chuckNorris()::fact);
+        stringCreator.add(faker.lebowski()::quote);
+        stringCreator.add(faker.friends()::quote);
+        stringCreator.add(faker.elderScrolls()::quote);
+        stringCreator.add(faker.gameOfThrones()::quote);
+        stringCreator.add(faker.howIMetYourMother()::quote);
+        stringCreator.add(faker.backToTheFuture()::quote);
+        stringCreator.add(faker.dune()::quote);
+        stringCreator.add(faker.hitchhikersGuideToTheGalaxy()::quote);
+        stringCreator.add(faker.rickAndMorty()::quote);
+        stringCreator.add(faker.yoda()::quote);
+        stringCreator.add(faker.robin()::quote);
+        stringCreator.add(faker.shakespeare()::asYouLikeItQuote);
+        stringCreator.add(faker.shakespeare()::hamletQuote);
+        stringCreator.add(faker.shakespeare()::kingRichardIIIQuote);
+        stringCreator.add(faker.shakespeare()::romeoAndJulietQuote);
+        stringCreator.add(faker.harryPotter()::quote);
+        stringCreator.add(faker.witcher()::quote);
+
+        System.out.println("Quote of the execution:");
+        StringCreator item = stringCreator.get(random.nextInt(stringCreator.size()));
+        System.out.println('`' + item.createString() + '`');
+    }
+}
